2793cf62f485f488874a49939b2520bce232a347
Vincent <vincentjames501@gmail.com>
2011-04-11 19:01:11 -0500

    -Added ability to see savings and shopping cart ticket as well as view all coupons in one window

--- src/main/java/gui/window/savings/ComputeSavingsWindow.java
+++ src/main/java/gui/window/savings/ComputeSavingsWindow.java
@@ -1,9 +1,12 @@
 package gui.window.savings;
 
+import hibernate.Coupon;
 import hibernate.ShoppingCart;
+import hibernate.ShoppingCartItem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,8 +16,8 @@ import java.awt.*;
  * To change this template use File | Settings | File Templates.
  */
 public class ComputeSavingsWindow extends JFrame {
-    private static final Integer WIDTH = 500;
-    private static final Integer HEIGHT = 330;
+    private static final Integer WIDTH = 525;
+    private static final Integer HEIGHT = 600;
 
     public ComputeSavingsWindow(ShoppingCart selectedShoppingCart) throws HeadlessException {
         setTitle("Savings For Shopping Cart");
@@ -22,15 +25,50 @@ public class ComputeSavingsWindow extends JFrame {
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.addTab("No Coupons Applied", createNoCouponsAppliedPane(selectedShoppingCart));
         tabbedPane.addTab("Coupons Applied", createCouponsAppliedPane(selectedShoppingCart));
+        add(tabbedPane);
     }
 
     private JPanel createCouponsAppliedPane(ShoppingCart selectedShoppingCart) {
-        JPanel panel = new JPanel();
-        return panel;
+        JPanel createCouponsAppliedPane = new JPanel();
+        String couponsAppliedText = "Item Name\t\tQuantity\t\tTotal\n\n";
+        float totalCartPrice = 0.0f;
+        for (ShoppingCartItem shoppingCartItem : selectedShoppingCart.getShoppingCartItems()) {
+            Set<Coupon> itemCoupons = shoppingCartItem.getItem().getItemCoupons();
+            float totalItemPrice = shoppingCartItem.getItemQuantity() * shoppingCartItem.getItem().getItemPrice();
+            totalCartPrice += totalItemPrice;
+            couponsAppliedText += shoppingCartItem.getItem().getItemName() + "\t" + shoppingCartItem.getItemQuantity() + "\t\t" +
+                    totalItemPrice + "\n";
+            for (Coupon itemCoupon : itemCoupons) {
+                if(itemCoupon.isCouponType()) {
+                    totalCartPrice -= (totalItemPrice*itemCoupon.getCouponValue()*0.01);
+                    couponsAppliedText += "\t\t-" + itemCoupon.getCouponValue() + "%\t\t-" + totalItemPrice*itemCoupon.getCouponValue()*0.01 + "\n";
+                } else {
+                    totalCartPrice -= (totalItemPrice-(totalItemPrice-itemCoupon.getCouponValue()));
+                    couponsAppliedText += "\t\t-$" + itemCoupon.getCouponValue() + "\t\t-" + (totalItemPrice-(totalItemPrice-itemCoupon.getCouponValue())) + "\n";
+                }
+            }
+        }
+        couponsAppliedText += "\n\nTotal:\t\t\t\t" + totalCartPrice;
+        JTextArea couponsAppliedTextArea = new JTextArea(couponsAppliedText);
+        couponsAppliedTextArea.setBackground(getBackground());
+        createCouponsAppliedPane.add(couponsAppliedTextArea);
+        return createCouponsAppliedPane;
     }
 
     private JPanel createNoCouponsAppliedPane(ShoppingCart selectedShoppingCart) {
-        JPanel panel = new JPanel();
-        return panel;
+        JPanel createNoCouponsAppliedPane = new JPanel();
+        String noCouponsAppliedText = "Item Name\t\tQuantity\t\tTotal\n\n";
+        float totalCartPrice = 0.0f;
+        for (ShoppingCartItem shoppingCartItem : selectedShoppingCart.getShoppingCartItems()) {
+            float totalItemPrice = shoppingCartItem.getItemQuantity() * shoppingCartItem.getItem().getItemPrice();
+            totalCartPrice += totalItemPrice;
+            noCouponsAppliedText += shoppingCartItem.getItem().getItemName() + "\t" + shoppingCartItem.getItemQuantity() + "\t\t" +
+                    totalItemPrice + "\n";
+        }
+        noCouponsAppliedText += "\n\nTotal:\t\t\t\t" + totalCartPrice;
+        JTextArea noCouponsAppliedTextArea = new JTextArea(noCouponsAppliedText);
+        noCouponsAppliedTextArea.setBackground(getBackground());
+        createNoCouponsAppliedPane.add(noCouponsAppliedTextArea);
+        return createNoCouponsAppliedPane;
     }
 }
